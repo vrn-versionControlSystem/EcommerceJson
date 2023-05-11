deaceaa097981bef861fb970bda8ce40dc6a37f5
Vasim <vrnadaf@gmail.com>
2023-05-11 08:33:10 +0530

    #7 display user signup page data

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Signup } from '../DataType';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
+
 export class SellerHomeComponent {
 
-    takedata:any;
+    takedata:undefined | Signup[];
+
+  constructor(private service:SellerService){}
+  ngOnInit()
+  {
+    this.displayAllSignupData();
+  }
+
+  displayAllSignupData()
+  {
+    this.service.userSignupGetData().subscribe((result)=>{
+      this.takedata=result;
+    });
+  }
+
 
 }
